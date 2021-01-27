b4c6786ea3300ca490768acf6e5eb7cfdb4d9921
SantiBert <santibertero34@gmail.com>
2021-01-26 21:05:45 -0300

    pros

--- src/components/Card/CardSection.js
+++ src/components/Card/CardSection.js
@@ -9,9 +9,9 @@ import CardHeader from "components/Card/CardHeader.js";
 import Button from "components/CustomButtons/Button.js";
 
 import { cardTitle } from "assets/jss/material-kit-react.js";
-import { on } from 'gulp';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-/*const WebSocket = require('ws');*/
+const WebSocket = require('ws');
 
 const styles = {
   cardTitle,
@@ -21,49 +21,47 @@ const useStyles = makeStyles(styles);
 
 export default function Cards({ name, key, sellvalue, buyvalue, icon }) {
   const classes = useStyles();
-  const [buyPrice, setBuyPrice] = useState(100);
-  /*useEffect(() => {
-    let ws2 = new WebSocket("wss://ws.bitstamp.net");
 
-    /*const ws2 = new W3CWebSocket('wss://ws.bitstamp.net');
+  const [buyPrice, setBuyPrice] = useState(100);
+  useEffect(() => {
+    const ws2 = new W3CWebSocket('wss://ws.bitstamp.net');
     console.log(key);
     var channelName = "live_trades_" + key + "usd";
-
     let channelData = {
       event: "bts:subscribe",
       data: {
         channel: channelName,
       }
     };
-
     //enviar datos al websocket, suscribo al channel
     ws2.onopen = function () {
       ws2.send(JSON.stringify(channelData));
     };
-
     ws2.onmessage = function (evt) {
       let response = JSON.parse(evt.data);
       console.log("este daisdjs");
     };
-
     ws2.onclose = function () {
       console.log("Websocket connection closed");
     };
-  });
-  */
+
+  }, [props.key]);
+
+
+
   return (
     <Card id={key} style={{ width: "20rem" }}>
       <CardHeader color="info"><i className={icon}></i> {name} </CardHeader>
       <CardBody>
         <h4 className={classes.cardTitle}>Compra</h4>
-        <Button color="primary" size="sm">{buyPrice}</Button>
+        <Button color="primary" size="sm">{buyvalue}</Button>
         <h4 className={classes.cardTitle}>Venta</h4>
         <Button color="primary" size="sm">{sellvalue}</Button>
       </CardBody>
     </Card>
   );
 }
-/* Un comentario JSX
+{/* Un comentario JSX 
 const WebSocket = require('ws');
 
 var usd_value = 153;
@@ -99,28 +97,28 @@ ws2.onmessage = function (evt) {
 };
 
 ws2.onclose = function () {
-    console.log("Websocket connection closed");
+    console.log("Websocket connection closed");      
 };
 
 function GetResult(price, percent){
     // realizar el calculo
-    //Recordar chequear por la moneda seleccionada
-    if (coinType == "ARS") {
+    //Recordar chequear por la moneda seleccionada    
+    if (coinType == "ARS") { 
         let conversion = price * usd_value
-        let result = conversion * (1 + percent);
+        let result = conversion * (1 + percent); 
         console.log(conversion);
-        return result;
-        }
+        return result;                  
+        }    
 }
 
 function NewMessage(data){
     if(data.channel == channelName){
         if(data.event == "trade"){
-            let sellValue = GetResult(data.data.price, sellPercent);
+            let sellValue = GetResult(data.data.price, sellPercent); 
             let buyValue = GetResult(data.data.price, buyPercent);
             //this.state.buyValue = sellValue;
             //this.state.buyValue = sellValue;
         }
     }
 }
-*/
+*/}
